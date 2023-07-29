79d1239d0d3e78f54208d5fa261b0151d60c1e5e
Rajesh Kumar <rajekumar7589@gmail.com>
2023-07-29 10:51:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Auth0Provider
+        domain="dev-e3auvekf0tr67sui.us.auth0.com"
+        clientId="JUeYk03lqloflxZPJpdSDlTeeVPZTagb"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <App />
+    </Auth0Provider>
+);
